5d30879f3cf284c7d519fa53d6cccc5889464fe7
Asad100225 <asadullahsk100225@gmail.com>
2025-07-03 09:54:07 +0530

    first commit

---
+++ first-agent/agent.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import Groq from "groq-sdk";
+dotenv.config();
+console.log("first agent");
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function callAgent() {
+  const completion = await groq.chat.completions
+    .create({
+      messages: [{
+        role:"assistant"
+      },
+        {
+          role: "user",
+          content: "Explain the importance of fast language models",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    })
+    // .then((chatCompletion) => {
+    //   console.log(chatCompletion.choices[0]?.message?.content || "");
+    // });
+
+    console.log(completion);
+    
+}
+
+callAgent();
